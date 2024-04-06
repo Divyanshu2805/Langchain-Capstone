0bd21c18717d905cb291d79cdd1ec9b571fcfb3b
Divyanshu Agrahari <98732490+Divyanshu2805@users.noreply.github.com>
2024-04-06 12:54:06 +0530

    saving messages in file

--- Tchat/main.py
+++ Tchat/main.py
@@ -2,13 +2,17 @@ from langchain.chat_models.openai import ChatOpenAI
 from langchain.chains.llm import LLMChain
 from langchain.prompts import MessagesPlaceholder, SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.memory.buffer import ConversationBufferMemory
+from langchain.memory import FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
-memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+memory = ConversationBufferMemory(
+    chat_memory=FileChatMessageHistory("messages.json"),
+    memory_key="messages",
+    return_messages=True)
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"],
