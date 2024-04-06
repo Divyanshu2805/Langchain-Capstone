7007fccbaa1421f39c796beb1c1395b10305ee18
Divyanshu Agrahari <98732490+Divyanshu2805@users.noreply.github.com>
2024-04-06 12:21:48 +0530

    Basic terminal Chatbot

---
+++ Tchat/main.py
@@ -0,0 +1,33 @@
+from langchain.chat_models.openai import ChatOpenAI
+from langchain.chains.llm import LLMChain
+from langchain.prompts import MessagesPlaceholder, SystemMessagePromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.memory.buffer import ConversationBufferMemory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+chat = ChatOpenAI()
+
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+    messages=[
+        MessagesPlaceholder(variable_name="messages"),
+        HumanMessagePromptTemplate.from_template("{content}")
+    ]
+)
+
+chain = LLMChain(
+    llm=chat,
+    prompt=prompt,
+    memory=memory
+)
+
+while True:
+    content = input(">> ")
+
+    # print(f"You entered: {content}")
+    result = chain({"content": content})
+
+    print(result["text"])
